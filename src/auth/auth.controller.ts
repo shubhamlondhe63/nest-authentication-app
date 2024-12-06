5be0be23a181e9c6b65874754d41c527b1e4d360
Shubham Londhe <shubhamlondhe63@gmail.com>
2024-12-06 17:53:16 +0530

    signup dto created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignupDto } from './dtos/signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  //Post sign up
+  @Post('signup') // auth/signup
+  signup(@Body() signupData: SignupDto) {
+    return 'User Signup';
+  }
 }
---
+++ src/auth/dtos/signup.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
+
+export class SignupDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/^(?=.*[0-9])/, {message: "Password must be at least one number"})
+  password: string;
+}
